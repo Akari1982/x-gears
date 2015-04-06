245c355e0c3296e73a899aa93813bd250c9b44d7
Akari1982 <bakari82@mail.ru>
2015-04-06 18:57:18 +0500

    Reverse update.

--- reversing/xeno/xeno_system/sprite/sprite_packet.cpp
+++ reversing/xeno/xeno_system/sprite/sprite_packet.cpp
@@ -1,224 +1,230 @@
 ////////////////////////////////
-// func1d3b8
-S4 = A0;
-frame_id = A1;
-S6 = A2;
+// func1e130
+S0 = A0;
+S1 = A1;
 
-sprite_1_address = w[S6];
-frame_id_offset = sprite_1_address + frame_id * 2;
-frame_id_address = sprite_1_address + hu[frame_id_offset];
-tile_offsets = frame_id_address + 4;
-number_of_tiles = bu[frame_id_address + 0] & 3f;
-tile_position_setting = frame_id_address + 4 + number_of_tiles * 2;
+8001E144	jal    func1dfe8 [$8001dfe8]
 
-[SP + 48] = w(S4);
+A0 = S0;
+A1 = S1;
+func1e264;
 
-[S4 + 36] = h((bu[frame_id_address + 3] * h[S4 + 2c]) >> c);
-[S4 + 38] = h((bu[A0 + 1] * h[S4 + 2c]) >> c);
-
-tex_y_off = bu[S6 + 6];
-g_abr = bu[S4 + 3c] >> 5;
-
-packet_header = w[S4 + 28]; // 2C808080
-
-S7 = 4;
-
-tile = 0;
-
-if( number_of_tiles != 0 )
+if( w[S0 + 3c] & 4 )
 {
-    V0 = w[S4 + 20];
-    sprite_data = w[V0 + 30];
-
-    L1d4bc:	; 8001D4BC
-        [sprite_data + 8] = b(0);
-        [sprite_data + 9] = b(0);
-        [sprite_data + 14] = w(w[sprite_data + 14] & ffffffdf); // remove flip vertical flag
-
-        L1d4d4:	; 8001D4D4
-        S1 = bu[tile_position_setting];
-        if( S1 & 80 )
-        {
-            tile_position_setting = tile_position_setting + 1;
-
-            if( S1 & 40 )
-            {
-                V0 = w[S4 + 20];
-                if( w[V0 + 34] == 0 ) // if memory not allocated
-                {
-                    A0 = 40; // allocate 40 byte
-                    A1 = 0;
-                    system_memory_allocate;
-                    V1 = w[S4 + 20];
-                    [V1 + 34] = w(V0);
-
-                    A0 = S4;
-                    func2332c; // set all fields to zero
-                }
-
-                S7 = S1 & 7;
-                V0 = w[S4 + 20];
-                V0 = w[V0 + 34];
-
-                if( S1 & 20 )
-                {
-                    [V0 + S7 * 8 + 0] = b(bu[tile_position_setting + 0]);
-                    [V0 + S7 * 8 + 1] = b(bu[tile_position_setting + 1]);
-                    tile_position_setting = tile_position_setting + 2;
-                }
-
-                if( S1 & 10 )
-                {
-                    [V0 + S7 * 8 + 6] = h(bu[tile_position_setting] << 4);
-                    tile_position_setting = tile_position_setting + 1;
-                }
-                else
-                {
-                    [V0 + S7 * 8 + 6] = h(0);
-                }
-            }
-            else
-            {
-                if( S1 & 01 ) // width increase
-                {
-                    [sprite_data + 8] = b(bu[tile_position_setting]);
-                    tile_position_setting = tile_position_setting + 1;
-                }
-                if( S1 & 02 ) // height increase
-                {
-                    [sprite_data + 9] = b(bu[tile_position_setting]);
-                    tile_position_setting = tile_position_setting + 1;
-                }
-                if( S1 & 04 ) // flip vertical
-                {
-                    [sprite_data + 14] = w(w[sprite_data + 14] | 00000020);
-                }
-            }
-            8001D588	j      L1d4d4 [$8001d4d4]
-        }
+    A0 = S0;
+    A1 = S1;
+    func1e834;
+}
+////////////////////////////////
 
 
 
-        tile_address = sprite_1_address + hu[tile_offsets];
-        tile_offsets = tile_offsets + 2;
-
-        S1 = bu[tile_address + 0];
-
-        if( S1 & 01 )
-        {
-            [sprite_data + 14] = w(w[sprite_data + 14] | 00000008);
-            tex_x_off = (hu[S6 + 4] & 3f) >> 1;
-        }
-        else
-        {
-            [sprite_data + 14] = w(w[sprite_data + 14] & fffffff7);
-            tex_x_off = (hu[S6 + 4] & 3f) >> 2;
-        }
+////////////////////////////////
+// func1e264
+S3 = A0;
+packet_addr = A1;
+addr = w[S3 + 20];
+addr2 = w[addr + 34];
+sprite_data = w[addr + 30];
+scale = (w[S3 + 40] >> 8) & 1f;
+number_of_tiles = (w[S3 + 40] >> 2) & 3f;
 
 
 
-        [sprite_data + 10] = w(packet_header);
-
-
-
-        A2 = bu[tile_position_setting] >> 4 & 03; // abr - Semi transparency mode
-
-        if( A2 != 0 )
-        {
-            [sprite_data + 13] = b(bu[sprite_data + 13] | 02); // semi-transparency on
-            A2 = A2 - 1;
-        }
-        else // 0.5xB+0.5 x F
-        {
-            A2 = g_abr;
-            if( A2 != 0 )
-            {
-                [sprite_data + 13] = b(bu[sprite_data + 13] | 02); // semi-transparency on
-                A2 = A2 - 1;
-            }
-        }
-
-        if( S1 & 10 )
-        {
-            tile_address = tile_address + 1;
-            S1 = (bu[tile_address] << 8) | S1;
-
-            A0 = S1 & 0001; // tp
-            A1 = A2; // abr
-            V0 = (S1 << 1) & 1c;
-            A2 = h[8004f15c + V0]; // tx
-            A3 = h[8004f15e + V0]; // ty and tr
-            func43894; // ((A0 & 3) << 7) | ((A1 & 3) << 5) | ((A3 & 0100) >> 4) | ((A2 & 03ff) >> 6) | ((A3 & 0200) << 2);
-            [sprite_data + a] = h(V0);
-
-            A0 = (S1 >> 1) + f0; // x
-            A1 = ((S1 >> 9) & f) + 1cc; // y
-            func438d0; // ((A1 << 6) | ((A0 >> 4) & 3f)) & ffff;
-            [sprite_data + c] = h(V0);
-        }
-        else
-        {
-            T0 = w[SP + 48];
-            V0 = w[T0 + 7c];
-            V1 = w[V0 + 18];
-            if( ( w[T0 + a8] & 0001 ) && ( V1 != 0 ) )
-            {
-                V0 = ((S1 << 1) & 1c) + V1;
-
-                tex_x_off = (hu[V0 + 0] & 3f) >> 2;
-                tex_y_off = hu[V0 + 2] & ff;
-
-                tx = (hu[V0 + 0] & 3ff) >> 6;
-                ty = (hu[V0 + 2] >> 4) & 0010;
-                abr = (A2 & 03) << 5;
-                tp = (S1 & 01) << 7;
-                tr = (hu[V0 + 2] & 0200) << 2;
-                [sprite_data + a] = h( tr | tp | abr | ty | tx);
-            }
-            else
-            {
-                tx = ((h[S6 + 4] + ((S1 << 5) & 01c0)) & 3ff) >> 6;
-                ty = (hu[S6 + 6] >> 4) & 0010;
-                abr = (A2 & 03) << 5;
-                tp = (S1 & 01) << 7;
-                tr = (hu[S6 + 6] & 0200) << 2; // Textured Rectangle X-Flip, Y-Flip
-                [sprite_data + a] = h( tr | tp | abr | ty | tx);
-            }
-
-            x = ((h[S6 + 8] + ((bu[tile_position_setting] & 0f) << 4)) >> 4) & 3f;
-            y = hu[S6 + a] << 6;
-            [sprite_data + c] = h(y | x);
-        }
-
-
-
-        [sprite_data + 4] = b(bu[tile_address + 1] + tex_x_off);
-        [sprite_data + 5] = b(bu[tile_address + 2] + tex_y_off);
-        [sprite_data + 6] = b(bu[tile_address + 3]);
-        [sprite_data + 7] = b(bu[tile_address + 4]);
-
-        [sprite_data + 14] = w((w[sprite_data + 14] & fffffff8) | S7 );
-        [sprite_data + 14] = w((w[sprite_data + 14] & ffffffef) | ((bu[tile_position_setting + 0] >> 2) & 10)); // flip horizontal if set
-
-        if( bu[frame_id_address + 0] & 80 ) // double presision
-        {
-            [sprite_data + 0] = h(hu[tile_position_setting + 1]);
-            [sprite_data + 2] = h(hu[tile_position_setting + 3]);
-            tile_position_setting = tile_position_setting + 2;
-        }
-        else
-        {
-            [sprite_data + 0] = h(b[tile_position_setting + 1]);
-            [sprite_data + 2] = h(b[tile_position_setting + 2]);
-        }
-
-
-
-        sprite_data = sprite_data + 18;
-        tile = tile + 1;
-        tile_position_setting = tile_position_setting + 3;
-    8001D910	bne    tile, number_of_tiles, L1d4bc [$8001d4bc]
+// offset for sprite
+offset_x = b[addr + 3c] << scale;
+offset_y = b[addr + 3d] << scale;
+if( w[S3 + ac] & 00000010 )
+{
+    offset_x = 0 - offset_x;
 }
 
-[S4 + 40] = w((w[S4 + 40] & ffffff03) | ((tile & 3f) << 2));
+
+
+if( w[80058c1c] + number_of_tiles * 28 >= w[80058bd0] || number_of_tiles == 0 )
+{
+    return;
+}
+
+
+
+tile = 0;
+add_id = -1;
+
+L1e324:	; 8001E324
+    V0 = w[sprite_data + 14] & 00000007;
+    if( add_id != V0 )
+    {
+        add_id = V0;
+        [SP + 78] = b((hu[8004f19c + add_id * 2] & bu[S3 + 3d]) < 1);
+
+        if( ( addr2 != 0 && ( ( hu[addr2 + add_id * 8 + 0] == 0 ) || ( h[addr2 + add_id * 8 + 6] != 0 ) )
+        {
+            S0 = b[addr2 + add_id * 8 + 0] << scale;
+            S1 = b[addr2 + add_id * 8 + 1] << scale;
+            if( w[S3 + 3c] & 00000008 )
+            {
+                S0 = 0 - b[addr2 + add_id * 8 + 0] << scale;
+            }
+
+            S1 = (S1 * h[S3 + 2c]) >> c;
+            S0 = (S0 * h[S3 + 2c]) >> c;
+
+            [SP + 48] = h(hu[addr2 + add_id * 8 + 2]); // rotation x
+            [SP + 4a] = h(hu[addr2 + add_id * 8 + 4]); // rotation y
+            [SP + 4c] = h(hu[addr2 + add_id * 8 + 6]); // rotation z
+            if( w[S3 + 3c] & 00000008 )
+            {
+                [SP + 4c] = h(0 - hu[addr2 + add_id * 8 + 6]);
+            }
+
+            A0 = SP + 48;
+            A1 = SP + 28;
+            func3f5e0; // rotaion matrix from rotation angles
+
+            // translation vector
+            [SP + 3c] = w(w[addr + 20] + S0);
+            [SP + 40] = w(w[addr + 24] + S1);
+            [SP + 44] = w(w[addr + 28]);
+
+            A0 = addr + c;
+            A1 = SP + 28;
+            func49724;  // multiply 2 matrix and set result as rotation matrix
+
+            A0 = SP + 28;
+            system_gte_set_translation_vector;
+        }
+        else
+        {
+            A0 = addr + c;
+            system_gte_set_rotation_matrix;
+
+            A0 = addr + c;
+            system_gte_set_translation_vector;
+        }
+    }
+
+
+
+    if( bu[SP + 78] != 0 )
+    {
+        S0 = w[80058c1c];
+        [80058c1c] = w(S0 + 28);
+
+        [S0 + 3] = b(09);
+        [S0 + 4] = w(w[sprite_data + 10]); // 2c808080 - Command + Color Vertex 0
+        [S0 + e] = h(h[sprite_data + c]); // clut id
+        [S0 + 16] = h(h[sprite_data + a]); // tpage
+
+        x0 = h[sprite_data + 0] << scale;
+        y0 = h[sprite_data + 2] << scale;
+        width = (bu[sprite_data + 6] + b[sprite_data + 8]) << scale;
+        height = (bu[sprite_data + 7] + b[sprite_data + 9]) << scale;
+
+        if( w[S3 + 3c] & 00000008 )
+        {
+            x0 = 0 - x0;
+            width = 0 - width;
+        }
+        if( w[S3 + 3c] & 00000010 )
+        {
+            y0 = 0 - y0;
+            height = 0 - height;
+        }
+
+        if( ( w[sprite_data + 14] & 00000010 ) == 0 ) // flip horizontal <>
+        {
+            [8004f23c + 00] = h(x0);
+            [8004f23c + 08] = h(x0 + width);
+            [8004f23c + 10] = h(x0 + width);
+            [8004f23c + 18] = h(x0);
+        }
+        else
+        {
+            [8004f23c + 00] = h(x0 + width);
+            [8004f23c + 08] = h(x0);
+            [8004f23c + 10] = h(x0);
+            [8004f23c + 18] = h(x0 + width);
+        }
+
+        if( ( w[sprite_data + 14] & 00000020 ) == 0 ) // flip vertical
+        {
+            [8004f23c + 02] = h(y0);
+            [8004f23c + 0a] = h(y0);
+            [8004f23c + 12] = h(y0 + height);
+            [8004f23c + 1a] = h(y0 + height);
+        }
+        else
+        {
+            [8004f23c + 02] = h(y0 + height);
+            [8004f23c + 0a] = h(y0 + height);
+            [8004f23c + 12] = h(y0);
+            [8004f23c + 1a] = h(y0);
+        }
+
+        [8004f23c + 00] = h(hu[8004f23c + 00] - offset_x);
+        [8004f23c + 08] = h(hu[8004f23c + 08] - offset_x);
+        [8004f23c + 10] = h(hu[8004f23c + 10] - offset_x);
+        [8004f23c + 18] = h(hu[8004f23c + 18] - offset_x);
+        [8004f23c + 02] = h(hu[8004f23c + 02] - offset_y);
+        [8004f23c + 0a] = h(hu[8004f23c + 0a] - offset_y);
+        [8004f23c + 12] = h(hu[8004f23c + 12] - offset_y);
+        [8004f23c + 1a] = h(hu[8004f23c + 1a] - offset_y);
+
+        A0 = 8004f23c + 00; // xyz0
+        A1 = 8004f23c + 08; // xyz1
+        A2 = 8004f23c + 10; // xyz3
+        A3 = 8004f23c + 18; // xyz2
+        A4 = S0 + 08; // xy0
+        A5 = S0 + 10; // xy1
+        A6 = S0 + 20; // xy3
+        A7 = S0 + 18; // xy2
+        A8 = SP + 5c; // Interpolation value for depth queing. (not used)
+        A9 = SP + 60; // return flags (not used)
+        func4a5e4; // transform 4 points by rotation matrix
+
+
+
+        // set up texture coordinates
+        u0 = bu[sprite_data + 4];
+        v0 = bu[sprite_data + 5];
+        width = bu[sprite_data + 6] - 1;
+        height = bu[sprite_data + 7] - 1;
+        if( h[S0 + 20] < h[S0 + 8] ) // x3 < x0
+        {
+            u0 = u0 - 1;
+            if( u0 < 0 )
+            {
+                u0 = 0;
+                width = width - 1;
+            }
+        }
+        [S0 + 0c] = b(u0); // u0
+        [S0 + 0d] = b(v0); // v0
+        [S0 + 14] = b(u0 + width); // u1
+        [S0 + 15] = b(v0); // v1
+        [S0 + 1c] = b(u0); // u2
+        [S0 + 1d] = b(v0 + height); // v2
+        [S0 + 24] = b(u0 + width); // u3
+        [S0 + 25] = b(v0 + height); // v3
+
+
+
+        if( w[S3 + 3c] & 00000800 )
+        {
+            [S0 + 0] = w((w[S0] & ff000000) | (w[packet_addr - add_id * 4] & 00ffffff));
+            [packet_addr - add_id * 4] = w((w[packet_addr - add_id * 4] & ff000000) | (S0 & 00ffffff));
+        }
+        else
+        {
+            [S0 + 0] = w((w[S0] & ff000000) | (w[packet_addr] & 00ffffff));
+            [packet_addr] = w((w[packet_addr] & ff000000) | (S0 & 00ffffff));
+        }
+    }
+
+
+
+    sprite_data = sprite_data + 18;
+    tile = tile + 1;
+8001E7F8	bne    tile, number_of_tiles, L1e324 [$8001e324]
 ////////////////////////////////
