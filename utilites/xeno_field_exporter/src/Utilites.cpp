9e6a8fac67ce3ea69c68c0654ae952f507ef601b
Akari <bakari82@mail.ru>
2016-09-02 10:44:51 +0500

    Reverse update.

---
+++ utilites/xeno_field_exporter/src/Utilites.cpp
@@ -0,0 +1,26 @@
+#include "Utilites.h"
+
+#include <OgreStringConverter.h>
+
+
+
+const Ogre::String
+GetFieldName( const int file_id )
+{
+    switch( file_id )
+    {
+        case 608: return "lahan_city";
+        case 616: return "lahan_tutorial";
+        case 618: return "lahan_shop";
+        case 620: return "lahan_civilian_house1";
+        case 622: return "lahan_civilian_house2";
+        case 624: return "lahan_bar1";
+        case 626: return "lahan_bar2";
+        case 628: return "lahan_alice_house1";
+        case 630: return "lahan_alice_house2";
+        case 632: return "lahan_chief_house1";
+        case 634: return "lahan_chief_house2";
+    }
+
+    return "";
+}
---
+++ utilites/xeno_field_exporter/src/Utilites.h
@@ -0,0 +1,13 @@
+#ifndef UTILITES_H
+#define UTILITES_H
+
+#include <OgreString.h>
+
+
+
+const Ogre::String
+GetFieldName( const int file_id );
+
+
+
+#endif // UTILITES_H
