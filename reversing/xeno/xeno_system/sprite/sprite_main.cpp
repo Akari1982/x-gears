c8b0c638ed81a291edfd0f960f894bca9d4994af
Akari1982 <bakari82@mail.ru>
2017-01-17 19:33:25 +0500

    Reverse.

--- reversing/xeno/xeno_system/sprite/sprite_main.cpp
+++ reversing/xeno/xeno_system/sprite/sprite_main.cpp
@@ -33,7 +33,7 @@ frame_id = A1;
 struct_110 = A2;
 
 struct_b4 = w[struct_164 + 20];
-struct_30 = S1 = w[struct_b4 + 30];
+struct_30 = w[struct_b4 + 30];
 sprite_file_1 = w[struct_110 + 0];
 sprite_file_2 = w[struct_110 + c];
 frame_data = sprite_file_1 + hu[sprite_file_1 + frame_id * 2];
@@ -71,7 +71,7 @@ if( w[struct_164 + 3c] & 40000000 )
         A2 = h[struct_110 + a]; // y
         A3 = h[sprite_file_2 + 0] * 10; // width
         A4 = 1; // height
-        func24fac(); // add texture lo load to vram queue (loaded via func24e3c())
+        func24fac(); // add texture lo load to vram queue
     }
 }
 
@@ -117,7 +117,6 @@ two_bytes = bu[frame_data + 0] & 80;
 [SP + 48] = w((hu[SP + 18] & 3f) << 2);
 [SP + 50] = w((hu[SP + 18] & 3f) << 1);
 [SP + 58] = w(bu[SP + 1a]);
-[SP + 40] = w(w[struct_164 + 28]);
 
 
 
@@ -130,214 +129,150 @@ if( number_of_tiles != 0 )
     FP = 0;
 
     L1dbf4:	; 8001DBF4
-        [S1 + 9] = b(0);
-        [S1 + 8] = b(0);
-        [S1 + 14] = w(w[struct_30 + 14] & ffffffdf);
+        [struct_30 + FP * 18 + 8] = b(0); // width increase
+        [struct_30 + FP * 18 + 9] = b(0); // height increase
+        [struct_30 + FP * 18 + 14] = w(w[struct_30 + FP * 18 + 14] & ffffffdf); // remove 0x000000020 - flip vertical
 
         L1dc0c:	; 8001DC0C
-        8001DC0C	lbu    s0, $0000(s2)
-        8001DC10	nop
-        8001DC14	andi   v0, s0, $0080
-        8001DC18	beq    v0, zero, L1dd34 [$8001dd34]
-        8001DC1C	andi   v0, s0, $0040
-        8001DC20	beq    v0, zero, L1dce4 [$8001dce4]
-        8001DC24	addiu  s2, s2, $0001
-        8001DC34	nop
-        8001DC3C	andi   s5, s0, $0007
-
-        if( w[struct_b4 + 34] == 0 )
+        S0 = bu[S2];
+        if( S0 & 80 )
         {
-            A0 = 40;
-            A1 = 0;
-            system_memory_allocate();
-            [struct_b4 + 34] = w(V0);
-
-            A0 = struct_164;
-            8001DC54	jal    func2332c [$8002332c]
-        }
-
-        8001DC5C	andi   v0, s0, $0020
-        8001DC60	beq    v0, zero, L1dc98 [$8001dc98]
-        8001DC64	sll    a0, s5, $03
-        8001DC6C	lbu    v1, $0000(s2)
-        V0 = w[struct_b4 + 34];
-        8001DC74	addiu  s2, s2, $0001
-        8001DC78	addu   v0, a0, v0
-        8001DC7C	sb     v1, $0000(v0)
-        8001DC84	lbu    v1, $0000(s2)
-        V0 = w[struct_b4 + 34];
-        8001DC8C	addiu  s2, s2, $0001
-        8001DC90	addu   a0, a0, v0
-        8001DC94	sb     v1, $0001(a0)
-
-        L1dc98:	; 8001DC98
-        8001DC98	andi   v0, s0, $0010
-        8001DC9C	beq    v0, zero, L1dcc8 [$8001dcc8]
-        8001DCA0	nop
-        8001DCA4	lbu    a0, $0000(s2)
-        8001DCA8	addiu  s2, s2, $0001
-        8001DCB0	sll    a0, a0, $04
-        V1 = w[struct_b4 + 34];
-        8001DCB8	sll    v0, s5, $03
-        8001DCBC	addu   v0, v0, v1
-        8001DCC0	j      L1dc0c [$8001dc0c]
-        8001DCC4	sh     a0, $0006(v0)
-
-        L1dcc8:	; 8001DCC8
-        V1 = w[struct_b4 + 34];
-        8001DCD4	sll    v0, s5, $03
-        8001DCD8	addu   v0, v0, v1
-        8001DCDC	j      L1dc0c [$8001dc0c]
-        8001DCE0	sh     zero, $0006(v0)
-
-        L1dce4:	; 8001DCE4
-        if( S0 & 4 )
-        {
-            [S1 + 14] = w(w[S1 + 14] | 00000020);
-        }
-        if( S0 & 1 )
-        {
-            [S1 + 8] = b(bu[S2 + 0]);
             S2 = S2 + 1;
-        }
-        if( S0 & 2 )
-        {
-            [S1 + 9] = b(bu[S2 + 0]);
-            S2 = S2 + 1;
-        }
-        8001DD2C	j      L1dc0c [$8001dc0c]
+            if( S0 & 40 )
+            {
+                S5 = S0 & 7;
 
-        L1dd34:	; 8001DD34
-        8001DD34	lhu    v0, $0002(s6)
-        8001DD38	lhu    v1, $0000(s6)
-        8001DD40	andi   a0, v0, $001f
-        8001DD44	sll    v1, v1, $02
-        8001DD48	sh     a0, $0020(sp)
-        8001DD4C	lhu    v0, $0002(s6)
-        S0 = sprite_file_1 + V1;
-        8001DD54	srl    v0, v0, $05
-        8001DD58	andi   v0, v0, $003f
-        8001DD5C	sh     v0, $0022(sp)
-        8001DD60	lhu    a3, $0002(s0)
-        8001DD64	nop
-        8001DD68	andi   v0, a3, $0001
-        8001DD6C	beq    v0, zero, L1dd94 [$8001dd94]
-        8001DD70	addiu  s6, s6, $0004
-        8001DD74	sll    v0, a0, $01
-        8001DD78	lw     t1, $0050(sp)
-        8001DD7C	lbu    v1, $0000(s0)
-        8001DD80	addu   a0, t1, v0
-        8001DD84	lw     v0, $0014(s1)
-        8001DD88	srl    s4, v1, $01
-        8001DD8C	j      L1ddb4 [$8001ddb4]
-        8001DD90	ori    v0, v0, $0008
+                if( w[struct_b4 + 34] == 0 )
+                {
+                    A0 = 40;
+                    A1 = 0;
+                    system_memory_allocate();
+                    [struct_b4 + 34] = w(V0);
 
-        L1dd94:	; 8001DD94
-        8001DD94	sll    v0, a0, $02
-        8001DD98	lw     t1, $0048(sp)
-        8001DD9C	lbu    v1, $0000(s0)
-        8001DDA0	addu   a0, t1, v0
-        8001DDA4	srl    s4, v1, $02
-        8001DDA8	lw     v0, $0014(s1)
-        8001DDAC	addiu  v1, zero, $fff7 (=-$9)
-        8001DDB0	and    v0, v0, v1
+                    A0 = struct_164;
+                    8001DC54	jal    func2332c [$8002332c]
+                }
 
-        L1ddb4:	; 8001DDB4
-        8001DDB4	sw     v0, $0014(s1)
-        8001DDB8	lw     v0, $0014(s1)
-        8001DDBC	addiu  v1, zero, $fff8 (=-$8)
-        8001DDC0	sb     a0, $0004(s1)
-        8001DDC4	and    v0, v0, v1
-        8001DDC8	or     v0, v0, s5
-        8001DDCC	sw     v0, $0014(s1)
-        8001DDD0	lbu    v0, $0022(sp)
-        8001DDD4	lw     t1, $0058(sp)
-        8001DDD8	lw     v1, $0014(s1)
-        8001DDDC	addu   v0, t1, v0
-        8001DDE0	sb     v0, $0005(s1)
-        8001DDE4	lbu    v0, $0000(s0)
-        8001DDE8	nop
-        8001DDEC	sb     v0, $0006(s1)
-        8001DDF0	lbu    v0, $0001(s0)
-        8001DDF4	addiu  a0, zero, $ffef (=-$11)
-        8001DDF8	sb     v0, $0007(s1)
-        8001DDFC	lbu    v0, $0000(s2)
-        8001DE00	and    v1, v1, a0
-        8001DE04	srl    v0, v0, $02
-        8001DE08	andi   v0, v0, $0010
-        8001DE0C	or     v1, v1, v0
-        8001DE10	sw     v1, $0014(s1)
-        8001DE18	lbu    v0, $0000(s2)
-        8001DE1C	lw     t1, $0040(sp)
-        8001DE20	srl    v0, v0, $04
-        A1 = V0 & 3;
-        A2 = bu[S2 + 0] & f;
-        8001DE30	sw     t1, $0010(s1)
+                if( S0 & 20 )
+                {
+                    V0 = w[struct_b4 + 34];
+                    [V0 + S5 * 8 + 1] = b(bu[S2]);
+                    S2 = S2 + 1;
+                    [V0 + S5 * 8 + 1] = b(bu[S2]);
+                    S2 = S2 + 1;
+                }
 
-
-        8001DE2C	bne    a1, zero, L1de48 [$8001de48]
-
-        A1 = hu[SP + 38];
-        if( ( w[SP + 38] << 10 ) != 0 )
-        {
-            L1de48:	; 8001DE48
-            A1 = A1 - 1;
-            [S1 + 13] = b(bu[S1 + 13] | 02);
+                if( S0 & 10 )
+                {
+                    V1 = w[struct_b4 + 34];
+                    [V1 + S5 * 8 + 6] = h(bu[S2] << 4);
+                    S2 = S2 + 1;
+                }
+                else
+                {
+                    V1 = w[struct_b4 + 34];
+                    [V1 + S5 * 8 + 6] = h(0);
+                }
+            }
+            else
+            {
+                if( S0 & 4 ) // flip vertical
+                {
+                    [struct_30 + FP * 18 + 14] = w(w[struct_30 + FP * 18 + 14] | 00000020);
+                }
+                if( S0 & 1 ) // width increase
+                {
+                    [struct_30 + FP * 18 + 8] = b(bu[S2 + 0]);
+                    S2 = S2 + 1;
+                }
+                if( S0 & 2 ) // height increase
+                {
+                    [struct_30 + FP * 18 + 9] = b(bu[S2 + 0]);
+                    S2 = S2 + 1;
+                }
+            }
+            8001DD2C	j      L1dc0c [$8001dc0c]
         }
 
-        8001DE58	andi   a0, a3, $0001
-        8001DE5C	sll    a0, a0, $07
-        8001DE60	andi   v0, a1, $0003
-        8001DE64	sll    v0, v0, $05
-        8001DE68	or     a0, a0, v0
-        8001DE6C	lhu    a1, $001a(sp)
-        V0 = hu[SP + 18];
-        8001DE74	srl    v1, a1, $04
-        8001DE78	andi   v1, v1, $0010
-        8001DE7C	or     a0, a0, v1
-        8001DE80	andi   v0, v0, $03ff
-        8001DE84	srl    v0, v0, $06
-        [S1 + a] = h(A0 | V0 | ((A1 & 200) << 2));
+        [SP + 20] = h(hu[S6 + 2] & 1f);
+        S0 = sprite_file_1 + hu[S6 + 0] * 4;
+        [SP + 22] = h((hu[S6 + 2] >> 5) & 3f);
 
-        A0 = A2 << 4;
-        A0 = h[struct_110 + 8] + A0; // clut x
-        A1 = h[struct_110 + a]; // clut y
-        8001DEA8	jal    func438d0 [$800438d0]
+        A3 = hu[S0 + 2];
 
-        // called many times on pc sprite change
 
-        [S1 + c] = h(V0);
-
-        A0 = S0 + 4;
-        A1 = ((hu[SP + 18] + hu[SP + 20]) << 10) >> 10;
-        A2 = ((hu[SP + 1a] + hu[SP + 22]) << 10) >> 10;
-        A3 = S4;
-        A4 = bu[S0 + 1];
-        func24fac();
-
-        if( two_bytes != 0 )
+        if( A3 & 0001 )
         {
-            8001DF00	lbu    v0, $0002(s2)
-            8001DF04	lbu    v1, $0001(s2)
-            8001DF08	sll    v0, v0, $18
-            8001DF0C	sra    v0, v0, $10
-            8001DF10	or     v1, v1, v0
-            8001DF14	sh     v1, $0000(s1)
-            8001DF18	lbu    v1, $0003(s2)
-            8001DF1C	lbu    v0, $0004(s2)
-            8001DF20	addiu  s2, s2, $0002
-            8001DF24	sll    v0, v0, $18
-            8001DF28	sra    v0, v0, $10
-            8001DF2C	or     v1, v1, v0
-            8001DF34	sh     v1, $0002(s1)
+            V0 = hu[SP + 20] << 1;
+            V1 = bu[S0];
+            A0 = w[SP + 50] + V0;
+            S4 = V1 >> 1;
+            [struct_30 + FP * 18 + 14] = w(w[struct_30 + FP * 18 + 14] | 00000008);
         }
         else
         {
-            [S1 + 0] = h(b[S2 + 1]);
-            [S1 + 2] = h(b[S2 + 2]);
+            V0 = hu[SP + 20] << 2;
+            V1 = bu[S0];
+            A0 = w[SP + 48] + V0;
+            S4 = V1 >> 2;
+            [struct_30 + FP * 18 + 14] = w(w[struct_30 + FP * 18 + 14] & fffffff7);
         }
 
-        S1 = S1 + 18;
+        [struct_30 + FP * 18 + 4] = b(A0); // texture x
+        [struct_30 + FP * 18 + 14] = w((w[struct_30 + FP * 18 + 14] & fffffff8) | S5);
+
+        [struct_30 + FP * 18 + 5] = b(w[SP + 58] + bu[SP + 22]); // texture y
+        [struct_30 + FP * 18 + 6] = b(bu[S0 + 0]); // width
+        [struct_30 + FP * 18 + 7] = b(bu[S0 + 1]); // height
+        [struct_30 + FP * 18 + 14] = w((w[struct_30 + FP * 18 + 14] & ffffffef) | ((bu[S2] >> 2) & 10));
+
+        A1 = (bu[S2] >> 4) & 3;
+        A2 = bu[S2 + 0] & f;
+        [struct_30 + FP * 18 + 10] = w(w[struct_164 + 28]); // command + color vertex 0
+
+        if( A1 != 0 )
+        {
+            A1 = A1 - 1;
+            [struct_30 + FP * 18 + 13] = b(bu[struct_30 + FP * 18 + 13] | 02);
+        }
+        else
+        {
+            A1 = hu[SP + 38];
+            if( A1 != 0 )
+            {
+                A1 = A1 - 1;
+                [struct_30 + FP * 18 + 13] = b(bu[struct_30 + FP * 18 + 13] | 02);
+            }
+        }
+
+        // tpage
+        [struct_30 + FP * 18 + a] = h(((A3 & 1) << 7) | ((A1 & 3) << 5) | ((hu[SP + 1a] >> 4) & 10) | ((hu[SP + 18] & 3ff) >> 6) | ((hu[SP + 1a] & 200) << 2));
+
+        A0 = h[struct_110 + 8] + (A2 << 4); // clut x
+        A1 = h[struct_110 + a]; // clut y
+        func438d0(); // ((A1 << 6) | ((A0 >> 4) & 3f)) & ffff
+        [struct_30 + FP * 18 + c] = h(V0); // clut id
+
+        A0 = S0 + 4; // address to load
+        A1 = ((hu[SP + 18] + hu[SP + 20]) << 10) >> 10; // x
+        A2 = ((hu[SP + 1a] + hu[SP + 22]) << 10) >> 10; // y
+        A3 = S4; // width
+        A4 = bu[S0 + 1]; // height
+        func24fac(); // add texture lo load to vram queue
+
+        if( two_bytes != 0 )
+        {
+            [struct_30 + FP * 18 + 0] = h((b[S2 + 2] << 8) | bu[S2 + 1]); // x tile pos
+            [struct_30 + FP * 18 + 2] = h((b[S2 + 4] << 8) | bu[S2 + 3]); // y tile pos
+            S2 = S2 + 2;
+        }
+        else
+        {
+            [struct_30 + FP * 18 + 0] = h(b[S2 + 1]); // x tile pos
+            [struct_30 + FP * 18 + 2] = h(b[S2 + 2]); // y tile pos
+        }
+
+        S6 = S6 + 4;
         S2 = S2 + 3;
         FP = FP + 1;
     8001DF70	bne    fp, number_of_tiles, L1dbf4 [$8001dbf4]
@@ -345,23 +280,16 @@ if( number_of_tiles != 0 )
 
 
 
-V1 = w[struct_164 + 40];
-8001DF7C	addiu  v0, zero, $ff03 (=-$fd)
-8001DF80	and    v1, v1, v0
-8001DF84	andi   v0, fp, $003f
-8001DF88	sll    v0, v0, $02
-8001DF8C	or     v1, v1, v0
-[struct_164 + 40] = w(V1);
+[struct_164 + 40] = w((w[struct_164 + 40] & ffffff03) | ((FP & 3f) << 2));
 
 
 
-// called on change direction of pc sprite
-A0 = 0;
-A1 = h[SP + 18];
-A2 = h[SP + 1a];
-A3 = bu[frame_data + 4];
-A4 = bu[frame_data + 5];
-func24fac();
+A0 = 0; // address to load
+A1 = h[SP + 18]; // x
+A2 = h[SP + 1a]; // y
+A3 = bu[frame_data + 4]; // width
+A4 = bu[frame_data + 5]; // height
+func24fac(); // add vram rect clean to queue
 ////////////////////////////////
 
 
@@ -539,7 +467,7 @@ if( number_of_tiles != 0 )
 
             A0 = (S1 >> 1) + f0; // x
             A1 = ((S1 >> 9) & f) + 1cc; // y
-            func438d0; // ((A1 << 6) | ((A0 >> 4) & 3f)) & ffff;
+            func438d0(); // ((A1 << 6) | ((A0 >> 4) & 3f)) & ffff;
             [tile_data + c] = h(V0);
         }
         else
